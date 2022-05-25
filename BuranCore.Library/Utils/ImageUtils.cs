eaa1aab66882b8c38d63762e43cb9b7c31d3af70
Osman Buran <osman.buran@gmail.com>
2022-05-25 15:42:02 +0300

    ok

---
+++ BuranCore.Library/Utils/ImageUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Buran.Core.Library.Utils
+{
+    public class ImageUtils
+    {
+        public async Task<string> GetImageAsBase64Url(string url)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var bytes = await client.GetByteArrayAsync(url);
+                    return "image/jpeg;base64," + Convert.ToBase64String(bytes);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return string.Empty;
+        }
+
+        public async Task<byte[]> GetImageAsBytes(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.GetByteArrayAsync(url);
+            }
+        }
+    }
+}
