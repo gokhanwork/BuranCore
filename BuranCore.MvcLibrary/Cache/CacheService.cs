6868a68195251ed3d6f764105c837ab3f541189e
Enver Pakol <48068986+enverpakol@users.noreply.github.com>
2022-08-13 15:27:44 +0300

    CacheService added (#4)
    Co-authored-by: Enver Pakol <epakol@outlook.com>

---
+++ BuranCore.MvcLibrary/Cache/CacheService.cs
@@ -0,0 +1,75 @@
+﻿using Buran.Core.Library.Reflection;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+namespace Buran.Core.MvcLibrary.Cache
+{
+    public class CacheService : ICacheService
+    {
+        protected readonly IMemoryCache _memoryCache;
+        public CacheService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        private void DeleteCache<T>() where T : class
+        {
+            string key = typeof(T).Name;
+            _memoryCache.Remove(key);
+        }
+
+        public List<T> GetList<T>() where T : class
+        {
+            List<T> cacheList = default;
+            string key = typeof(T).Name;
+            _memoryCache.TryGetValue(key, out cacheList);
+
+            return cacheList;
+        }
+
+        public T GetItem<T>(int id) where T : class
+        {
+            var list = GetList<T>();
+            T entry = default;
+            entry = list.AsQueryable().Where($"Id=={id}").FirstOrDefault();
+            return entry;
+        }
+
+        public void Insert<T>(T item) where T : class
+        {
+            var list = GetList<T>();
+            list.Add(item);
+            DeleteCache<T>();
+            Load(list);
+        }
+
+        public void Load<T>(List<T> value) where T : class
+        {
+            string key = typeof(T).Name;
+            _memoryCache.Set(key, value, new MemoryCacheEntryOptions
+            {
+                Priority = CacheItemPriority.NeverRemove,
+            });
+        }
+
+        public void RemoveItem<T>(T item) where T : class
+        {
+            var list = GetList<T>();
+
+            var a = Digger.GetObjectValue(item, "Id");
+            var removeItem = list.AsQueryable().Where($"Id=={a}").FirstOrDefault();
+            list.Remove(removeItem);
+            DeleteCache<T>();
+            Load(list);
+        }
+        public void Update<T>(T item) where T : class
+        {
+            RemoveItem(item);
+            Insert(item);
+        }
+    }
+}
---
+++ BuranCore.MvcLibrary/Cache/ICacheService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buran.Core.MvcLibrary.Cache
+{
+    public interface ICacheService
+    {
+        List<T> GetList<T>() where T : class;
+        void Load<T>(List<T> value) where T : class;
+        T GetItem<T>(int id) where T : class;
+        void RemoveItem<T>(T item) where T : class;
+        void Update<T>(T item) where T : class;
+        void Insert<T>(T item) where T : class;
+    }
+}
