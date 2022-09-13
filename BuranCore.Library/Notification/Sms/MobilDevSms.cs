9a96bead2840a77d9d78a27f802b7c93febe1c9c
Gökhan Polat <gokhanplt.com@gmail.com>
2022-09-13 11:05:39 +0300

    MobilDev SMS Entegrasyonu

---
+++ BuranCore.Library/Notification/Sms/MobilDevSms.cs
@@ -0,0 +1,69 @@
+﻿using Buran.Core.Library.Http;
+using Buran.Core.Library.LogUtil;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Buran.Core.Library.Notification.Sms
+{
+    public class MobilDevSmsResponse
+    {
+        public string Id { get; set; }
+        public string Err { get; set; }
+    }
+
+    public class MobilDevSms
+    {
+        private readonly string _apiKey;
+        private readonly string _apiSecret;
+        private readonly string _from;
+
+        public MobilDevSms(string apiKey, string apiSecret)
+        {
+            _apiKey = apiKey;
+            _apiSecret = apiSecret;
+        }
+
+        public MobilDevSmsResponse SendSms(string toPhone, string msg)
+        {
+            try
+            {
+                var xmlData = $@"<MainmsgBody>
+    <UserName>{_apiKey}</UserName>
+    <PassWord>{_apiSecret}</PassWord>
+    <Action>0</Action>
+    <Mesgbody>{msg}</Mesgbody>
+    <Numbers>{toPhone}</Numbers>
+    <AccountId></AccountId>
+    <Originator></Originator>
+    <Blacklist>1</Blacklist>
+    <SDate></SDate>
+    <EDate></EDate>
+    <Encoding>0</Encoding>
+    <MessageType>N</MessageType>
+    <RecipientType></RecipientType>
+</MainmsgBody>
+";
+                var client = new WebRequest2();
+                var response = client.PostString("https://xmlapi.mobildev.com", xmlData);
+
+                var ss = response.Split(' ');
+                var res = new MobilDevSmsResponse();
+                if (ss.Length <= 2)
+                {
+                    int.TryParse(ss[0], out int i);
+                    if (i == 0)
+                    {
+                        res.Id = ss[1];
+                    }
+                }
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return new MobilDevSmsResponse { Err = Logger.GetErrorMessage(ex) };
+            }
+        }
+    }
+}
--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Buran.Core.Library.Notification.Sms;
+
+
+
+#region MobilDevSMS-Test
+var test = new  MobilDevSms("apiKey","apiSecret");
+var result = test.SendSms("+905321632051", "Deneme Sms VeriPlus");
+Console.WriteLine("Sms Gönderildi "+ result.Id);
 Console.ReadLine();
+#endregion
+
